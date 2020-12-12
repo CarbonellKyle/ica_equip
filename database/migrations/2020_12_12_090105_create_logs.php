9e4334e4aa5683014ab95ff1e401690bdc97c1b4
CarbonellKyle <s.carbonell@cmu.edu.ph>
2020-12-12 17:37:05 +0800

    added migrations

---
+++ database/migrations/2020_12_12_090105_create_logs.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLogs extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('logs', function (Blueprint $table) {
+            $table->id('logs_id');
+            $table->string('borrower_name');
+            $table->foreignId('equipment_id');
+            $table->dateTime('datetime_borrowed');
+            $table->dateTime('datetime_returned');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('logs');
+    }
+}
